425f7d7f1456de62568ab5a981c902c5130486b6
AP <endroo04@gmail.com>
2026-03-27 00:54:23 -0400

    Added ngrok host to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['27b7-2603-6011-2df0-3740-e1a4-d4da-f1ec-7814.ngrok-free.app']
+  },
 })
